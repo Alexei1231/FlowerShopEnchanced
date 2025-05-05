71604c0039e3588798f23be28e739589b3605780
Alexei1231 <popowalex4@gmail.com>
2025-05-05 20:28:03 +0200

    Gui - created classes

---
+++ src/MainGUI.java
@@ -0,0 +1,10 @@
+import gui.FlowerMenu;
+import gui.MainMenu;
+
+public class MainGUI {
+    public static void main(String[] args) {
+        MainMenu mainMenu = new MainMenu();
+        mainMenu.setVisible(true);
+
+    }
+}
