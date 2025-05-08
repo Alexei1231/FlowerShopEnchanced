d4445c0089cf03c5efa8901a162b2b076f2fa0e8
Alexei1231 <popowalex4@gmail.com>
2025-05-08 23:16:33 +0200

    Finished Project

--- src/MainGUI.java
+++ src/MainGUI.java
@@ -1,12 +1,22 @@
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import gui.FlowerMenu;
 import gui.MainMenu;
 import model.Employee;
+import model.Flower;
 import model.Shop;
 import files.FileOperations;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+
 public class MainGUI {
     public static void main(String[] args) {
-        Shop flowerShop = new Shop(0, "defaultShopName", "defaultShopAdress");
+        Shop flowerShop = new Shop(0, "defaultShopName", "defaultShopAddress");
 
         FileOperations fileOperations = new FileOperations();
         flowerShop.setEmployees(fileOperations.readEmployees());
@@ -14,6 +24,5 @@ public class MainGUI {
 
         MainMenu mainMenu = new MainMenu(flowerShop);
         mainMenu.setVisible(true);
-
     }
 }
